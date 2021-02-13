5a2037af5b4291ab12bab6c88855d448707e5154
micah hawman <micah.hawman@gmail.com>
2021-02-13 15:00:04 -0600

    support for sprite extracting.

--- BeyondChaosUpdater/BeyondChaosUpdater.py
+++ BeyondChaosUpdater/BeyondChaosUpdater.py
@@ -1,21 +1,51 @@
 from zipfile import ZipFile
 import subprocess
 import Constants
+from pathlib import Path
+
 
 def main():
     print(Constants.UpdaterLaunched)
-    with ZipFile('BeyondChaos.zip', 'r') as zipObj:
-       print(Constants.UpdaterUnzipping)
-       # Extract all the contents of zip file in different directory
-       zipObj.extractall()
-       #wait 3 seconds
-       time.sleep(3)
-       print(Constants.UpdaterCompleted)
-       print(Constants.UpdaterClosing)
-       subprocess.run("BeyondChaos.exe", shell=True)
-       #wait 3 seconds
-       time.sleep(3)
-       SystemExit();
+    updateBC()
+    updateSprites()
+    launchBC()
+
+def launchBC():
+     print(Constants.UpdaterClosing)
+     subprocess.run("BeyondChaos.exe", shell=True)
+     #wait 3 seconds
+     time.sleep(3)
+
+def updateSprites():
+    my_file = Path("Sprites.zip")
+    if my_file.is_file():
+        with ZipFile('Sprites.zip', 'r') as zipObj:
+           print(Constants.UpdaterUnzipping)
+           # Extract all the contents of zip file in different directory
+           zipObj.extractall("/Custom/")
+           #wait 3 seconds
+           time.sleep(3)
+           print(Constants.UpdaterCompleted)
+          
+    
+    else:
+        print(Constants.UpdaterSpriteError)
+        time.sleep(3)
+
+def updateBC():
+    my_file = Path("BeyondChaos.zip")
+    if my_file.is_file():
+        with ZipFile('BeyondChaos.zip', 'r') as zipObj:
+           print(Constants.UpdaterUnzipping)
+           # Extract all the contents of zip file in different directory
+           zipObj.extractall()
+           #wait 3 seconds
+           time.sleep(3)
+           print(Constants.UpdaterCompleted)
+    
+    else:
+        print(Constants.UpdaterBCError)
+        time.sleep(3)
 
 if __name__ == '__main__':
    main()
--- BeyondChaosUpdater/Constants.py
+++ BeyondChaosUpdater/Constants.py
@@ -1,4 +1,9 @@
-UpdaterLaunched = "Updating the application, please do not close this window."
-UpdaterUnzipping = "Updater is unpacking new files";
-UpdaterCompleted = "Update Completed!"
+UpdaterLaunched = "Welcome to the updater, we are starting things up, please do not close this window!"
+UpdateBC = "Updater is updating the randomizer core";
+UpdateBCDone = "Updater has updated the randomizer core";
+UpdateSprites = "Updater is updating the randomizer sprites";
+UpdateSpriteDone = "Updater has updated the randomizer sprites";
+UpdaterCompleted = "Update completed all update tasks!"
 UpdaterClosing = "Launching Beyond Chaos"
+UpdaterBCError = "BeyondChaos.zip does not exist, if this an error please report it to the development discord channel."
+UpdaterSpriteError = "Sprites.zip does not exist, if this an error please report it to the development discord channel."
